7620618453d8a32ce4e7b267a892641cb6e1c788
Ignetos <krisnain.dev@gmail.com>
2025-01-18 19:21:17 +0700

    add class for drawing circle

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -15,6 +17,8 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
